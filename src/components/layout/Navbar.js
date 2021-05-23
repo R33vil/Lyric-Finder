230176491b745416b5735ad20880cdcb1a9c8bfc
Andrew Shenkosky <t33m1o7lol@gmail.com>
2021-05-23 16:19:02 -0500

    init

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+        <span className="span.navbar-brand.mb-0.h1.mx-auto">LyricFinder</span>
+    </nav>);
+};
+
+export default Navbar;
